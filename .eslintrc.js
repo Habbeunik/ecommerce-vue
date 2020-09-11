6fa065ec7f944242401969c1e73e43dd3ccf9d4a
Kumapayi Abbey <habbeyunik@gmail.com>
2020-09-11 09:47:11 +0100

    setup prettier with eslint integrations

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,22 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "plugin:vue/essential",
-        "airbnb-base"
-    ],
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "parser": "@typescript-eslint/parser",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
+	env: {
+		browser: true
+		// es2021: true,
+	},
+	extends: [
+		'plugin:vue/essential',
+		'airbnb-base',
+		'plugin:vue/vue3-recommended',
+		'prettier',
+		'prettier/vue',
+		'plugin:prettier/recommended'
+	],
+	parser: 'vue-eslint-parser',
+	parserOptions: {
+		ecmaVersion: 12,
+		parser: 'babel-eslint',
+		sourceType: 'module'
+	},
+	plugins: ['vue', '@typescript-eslint'],
+	rules: {}
 };
